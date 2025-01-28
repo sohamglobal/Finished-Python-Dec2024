5c65af4b66480525622dda52bedd9e6614d49df4
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-01-28 15:55:57 +0530

    Update TypeSearch.py

--- Flask-REST/TypeSearch.py
+++ Flask-REST/TypeSearch.py
@@ -9,7 +9,7 @@ class AccountTypeInfo(Resource):
     def get(self,typ):
         lst=[]
         
-        con=pymysql.connect(host='mysql-java-javaee-project.c.aivencloud.com',port=19179,user='avnadmin',password='AVNS_TEZ17S2CIEzgqRcBnb0',database='sharayudb')
+        con=pymysql.connect(host='mysql-java-javaee-project.c.aivencloud.com',port=19179,user='avnadmin',password='AVNS_ethan913',database='sharayudb')
         curs=con.cursor()
         curs.execute("select * from accounts where acctype='%s'" %typ)
         data=curs.fetchall()
