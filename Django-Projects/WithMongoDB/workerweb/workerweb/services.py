94c62fe61211ea6bc853cbbe4c2137c45cd36b2b
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-01-28 15:42:11 +0530

    Update services.py

--- Django-Projects/WithMongoDB/workerweb/workerweb/services.py
+++ Django-Projects/WithMongoDB/workerweb/workerweb/services.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 class WorkerServices:
     def addnewworker(self,dic):
         try:
-            client=MongoClient("mongodb+srv://praffull:mongodb913@ethancluster.nz1sx.mongodb.net/?retryWrites=true&w=majority&appName=EthanCluster")
+            client=MongoClient("mongodb+srv://praffull:ethan913@ethancluster.nz1sx.mongodb.net/?retryWrites=true&w=majority&appName=EthanCluster")
             db=client["spiderdb"]
             coll=db["workers"]
             coll.insert_one(dic)
