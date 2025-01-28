cd970b07505c6ca3bbda5bcb53728962312acd65
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-01-28 15:55:31 +0530

    Update NewCarPOST.py

--- Flask-REST/NewCarPOST.py
+++ Flask-REST/NewCarPOST.py
@@ -11,7 +11,7 @@ def newcar():
     pr=float(request.form.get('price'))
     dic={}
     try:
-        con=pymysql.connect(host='mysql-java-javaee-project.c.aivencloud.com',port=19179,user='avnadmin',password='AVNS_TEZ17S2CIEzgqRcBnb0',database='sharayudb')
+        con=pymysql.connect(host='mysql-java-javaee-project.c.aivencloud.com',port=19179,user='avnadmin',password='AVNS_ethan913',database='sharayudb')
         curs=con.cursor()
         curs.execute("insert into cars values('%s','%s','%s',%.2f)" %(cid,nm,co,pr))
         con.commit()
